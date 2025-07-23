44f4392980b842022cb16f494663991e1f11f4e0
ningnong239 <ningnong2539@gmail.com>
2025-07-23 15:30:38 +0700

    feat: add answer to javascript skill checkpoint1

--- question-3.js
+++ question-3.js
@@ -1,3 +1,22 @@
 // Question #3
 let userPassword = "";
 // เริ่มเขียนโค้ดตรงนี้
+function checkPasswordStrength(userPassword) {
+ let password = userPassword.length;
+
+  if (password < 6) {
+    return "Weak";
+  } else if (password >= 6 && password <= 10) {
+    return "Medium";
+  } else {
+    return "Strong";
+  }
+}
+userPassword = "sssmnallwadqq";
+console.log(checkPasswordStrength(userPassword)); // "Strong"
+
+userPassword = "Techup";
+console.log(checkPasswordStrength(userPassword)); // "Medium"
+
+userPassword = "abcde";
+console.log(checkPasswordStrength(userPassword)); // "Weak"
